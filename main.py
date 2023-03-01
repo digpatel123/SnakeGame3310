840d29b6eb0d79ab4d31ee857d1bca200b59ba73
digpatel123 <digpatel123@gmail.com>
2023-03-01 18:55:53 +0000

    Created a method to extend the snake as it hits the food.

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from snake import Snake
 from food import Food
 from scoreboard import ScoreBoard
 import time
+
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.title("Snake Game")
@@ -25,11 +26,21 @@ while game_is_on:
     time.sleep(0.1)
     snake.move()
 
+    # Detects if the snake hits the food.
     if snake.head.distance(food) < 15:
         food.refresh()
+        snake.extend_snake()
         scoreboard.refresh_score()
 
+    # Detects if the snake hits the wall.
     if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
         game_is_on = False
         scoreboard.game_over()
+
+    # Detects if the snake hits its own tail.
+    for seg in snake.segments[1:]:
+
+        if snake.head.distance(seg) < 10:
+            game_is_on = False
+            scoreboard.game_over()
 screen.exitonclick()
--- snake.py
+++ snake.py
@@ -16,11 +16,20 @@ class Snake:
 
     def create_snake(self):
         for position in STARTING_POSITIONS:
-            tim = Turtle("square")
-            tim.color("white")
-            tim.penup()
-            tim.goto(position)
-            self.segments.append(tim)
+            self.add_segment(position)
+
+
+    def add_segment(self, position):
+        tim = Turtle("square")
+        tim.color("white")
+        tim.penup()
+        tim.goto(position)
+        self.segments.append(tim)
+
+    def extend_snake(self):
+        self.add_segment(self.segments[-1].position())
+
+
 
     def move(self):
         for seg_num in range(len(self.segments) - 1, 0, -1):
